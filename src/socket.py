398fd18eacbca3531f0d3e5b67c4ef8ed3975de4
undercoverTentative <47453376+undercoverTentative@users.noreply.github.com>
2021-02-03 19:34:26 +0100

    testing the socket

--- src/socket.py
+++ src/socket.py
@@ -1,1 +1,18 @@
 import socket
+
+
+class Socket:
+    """class of creating a socket to preform a scan of the volatile ports"""
+
+
+
+    def __init__(self, host, port):
+        self.ipAddress = '127.0.0.1'
+        self.port = 55440
+
+
+
+
+    def createSocket():
+        addr = (self.ipAddress,self.port)
+        print(addr)
