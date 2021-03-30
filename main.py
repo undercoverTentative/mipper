814537f2ae92f9ddf0e41ad713508c1330511658
undercoverTentative <47453376+undercoverTentative@users.noreply.github.com>
2021-03-30 16:59:36 +0200

    this way flask works

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+from flask import Flask,render_template,request,send_from_directory,send_file
+import sys,os,sqlite3
+from threading import Thread,Lock
 from src.create_socket import create_Socket
 from src.print_output import Output
 from src.create_output import PrintOutput
 from src.read_db import ReadDB
-from flask import Flask,render_template,request,send_from_directory,send_file
-from threading import Thread,Lock
-import sys,os,queue,sqlite3
+import queue
+
+
+
 
 lock = Lock()
+app = Flask(__name__)
 
-def main(host,Sport,Eport,scantype,threads=0,jsonout=0,xmlout=0):
+if __name__ == '__main__':
+    app.run(debug=True)
+
+def run_mipper(host,Sport,Eport,scantype,threads=0,jsonout=0,xmlout=0):
     Files = PrintOutput(host,scantype)
     q = queue.Queue()
     if threads > 0:
@@ -23,31 +31,19 @@ def main(host,Sport,Eport,scantype,threads=0,jsonout=0,xmlout=0):
                 threadlist.append(t)
             for i in threadlist:
                 i.join()
-    else:
-        ThreadScan(host,Sport,Eport,scantype,q)
-
-    for i in range(q.qsize()):
-        data = q.get()
-        if jsonout == 1:
-            Files.AppendToJson(data[1],data[2])
-        if xmlout == 1:
-            Files.AppendToXml(data[1],data[2])
-        Files.AppendToDb(data[1],data[2])
-    if jsonout == 1:
-        Files.writeJsonOutput()
-    if xmlout == 1:
-        Files.writeXmlOutput()
-
-def readout(dbname):
-    result = ReadDB(FileName=dbname)
-
-    if result.DBexist == False:
-        print("Database doesnt exist! please check in the root folder if the given name of the database is present")
-    if result.DBexist == True:
-        data = result.GetDataFromDb(data_get="host,port,result,scantype")
-        output = Output(host=data[1][0])
-        for d in data:
-            output.printout(d[2],d[1])
+            else:
+                ThreadScan(host,Sport,Eport,scantype,q)
+            for i in range(q.qsize()):
+                data = q.get()
+                if jsonout == 1:
+                    Files.AppendToJson(data[1],data[2])
+                if xmlout == 1:
+                    Files.AppendToXml(data[1],data[2])
+                    Files.AppendToDb(data[1],data[2])
+                if jsonout == 1:
+                    Files.writeJsonOutput()
+                if xmlout == 1:
+                    Files.writeXmlOutput()
 
 
 def ThreadScan(dsthost,Sport,Eport,scantyp,que):
@@ -88,17 +84,8 @@ def SplitThreads(Sport,Eport,Threads):
         list.append(leftoverdata)
     return list
 
+@app.route("/")
 def hello():
-    return "Hello world!"
-
-
-#if __name__ == '__main__':
-    #main("192.168.1.1",0,100,scantype="TCPportscan",threads=20,jsonout=0,xmlout=0)
-    #readout("Default")
-app = Flask(__name__)
-
-@app.route('/')
-def hello(name=None):
     return render_template('test.html')
 
 @app.route('/test', methods=['GET','POST'])
@@ -109,10 +96,10 @@ def get_value():
         if int(request.form['sport']) > 0 and int(request.form['sport']) <= 65535:
             if int(request.form['eport']) > 0 and int(request.form['eport']) <= 65535:
                 if int(request.form['sport']) < int(request.form['eport']):
-                    main(request.form['Host'],int(request.form['sport']),int(request.form['eport']),request.form['scan'],int(request.form['threads']),int(request.form['jsonout']),int(request.form['xmlout']))
+                    run_mipper(request.form['Host'],int(request.form['sport']),int(request.form['eport']),request.form['scan'],int(request.form['threads']),int(request.form['jsonout']),int(request.form['xmlout']))
                     con = sqlite3.connect("Default.db")
                     cursor = con.cursor()
-                    cursor.execute("SELECT * FROM scan")
+                    cursor.execute("SELECT * FROM scan ORDER BY port ASC")
                     data = cursor.fetchall()
                     return render_template('result.html', data=data)
                 else:
